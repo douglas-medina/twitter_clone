099b3515dab8bb45e17d5299aa7de67e7e18f354
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-07-17 18:24:56 -0300

    🐞 FIX:  INSSUE #2

--- app/pages/create_tweet_page.py
+++ app/pages/create_tweet_page.py
@@ -12,13 +12,22 @@ def create_tweet(content, username, password):
 
 def show():
     st.subheader('Create Tweet')
-    new_tweet = st.text_area('Tweet Content', key='tweet_content')
+
+    if 'tweet_content_temp' not in st.session_state:
+        st.session_state.tweet_content_temp = ''
+
+    if 'tweet_created' in st.session_state:
+        if st.session_state.tweet_created:
+            st.success('Tweet created successfully!')
+            st.session_state.tweet_content_temp = ''
+            st.session_state.tweet_created = False
+            st.experimental_rerun()
+
+    new_tweet = st.text_area('Tweet Content', value=st.session_state.tweet_content_temp, key='tweet_content')
 
     if st.button('Create Tweet'):
         response = create_tweet(new_tweet, st.session_state.username, st.session_state.password)
         if response.status_code == 201:
-            st.success('Tweet created successfully!')
-            st.session_state['tweet_content'] = ''  # Clear the text area
-            st.experimental_rerun()  # Refresh the page to update the feed
+            st.session_state.tweet_created = True
         else:
             st.error(f'Failed to create tweet. Error: {response.status_code} - {response.text}')
