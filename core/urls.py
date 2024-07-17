d2ec7948cda2b7bed3952055cc7e0949f561ec99
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-07-17 13:12:03 -0300

    django testes

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('tweets/', TweetListCreateView.as_view(), name='tweets'),
-    path('follow/<int:pk>/', FollowUnfollowView.as_view(), name='follow_unfollow'),
+    path('api/follow/<int:pk>/', FollowUnfollowView.as_view(), name='follow_unfollow'),
     path('feed/', FeedView.as_view(), name='feed'),
 ]
